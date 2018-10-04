1b3939c6af0e69eb03051060627c523ce07fba85
lotso <glendonshih@gmail.com>
2018-10-04 15:27:23 +0800

    Update random_produce.c

--- random_produce.c
+++ random_produce.c
@@ -1,12 +1,17 @@
 #include "random_produce.h"
 
+//y[] is yi=yi+Rj(it)xdt
+//normalize is DO normalize or NOT
+//Y is packet count
 
 double H_function(double y[], char normalize, int Y)
 {
     int i;
     double s=0;
     
-    for(i=0;i<20;i++) s += exp(y[i]);
+    for(i=0;i<20;i++){
+      s += exp(y[i]);
+    } 
     s = -log10(0.05*s);
     if(normalize) s /= log10(Y);
     return s;
