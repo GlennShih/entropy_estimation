b2371ff5c1eb75f84ffaf874ce50607f9f667adb
lotso <glendonshih@gmail.com>
2018-10-05 17:06:18 +0800

    Add files via upload

---
+++ random_produce.h
@@ -0,0 +1,10 @@
+#ifndef RANDOM_PRODUCE_H
+#define RANDOM_PRODUCE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+double H_function(double y[], char normalize, int Y);
+#endif
