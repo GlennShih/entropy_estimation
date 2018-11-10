e82423f23f59e64e04af0e4b84fc70d4641b567b
glennshih <glendonshih@gmail.com>
2018-11-10 16:58:23 +0800

    segamentation fault of sourcedemo_noXOR_RNG_multiTable.c

--- sourcedemo_noXOR_RNG_multiTable.c
+++ sourcedemo_noXOR_RNG_multiTable.c
@@ -5,6 +5,7 @@
  */
 #include "libtrace.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <assert.h>
 #include <getopt.h>
@@ -47,8 +48,8 @@ int main(int argc, char **argv)
         libtrace_packet_t *packet = NULL;
 
 	/* Ensure we have at least one argument after the program name */
-        if (argc < 4) {
-                fprintf(stderr, "Usage: %s  <trace file> <packet count> <array file>\n", argv[0]);
+        if (argc < 5) {
+                fprintf(stderr, "Usage: %s  <trace file> <packet count> <array file1> <array file2>\n", argv[0]);
                 return 1;
         }
 
@@ -74,71 +75,100 @@ int main(int argc, char **argv)
                 return 1;
         }
 
-	uint32_t i=0,Y=0;
-	double z[K_VALUE]={0.0}, lookup_table[524288], tmp2=0.0;
+	uint32_t i=0,Y=0,lineA=0, lineB=0;
+	long double z[K_VALUE]={0.0}, tmp2=0.0;
+    long double lookup_tableA[524288], lookup_tableB[524288];
 	uint32_t int_ip,tmp;
 	long m = atol(argv[2]);
-	int size;// = atol(argv[4]);
 
 	//read array table
-	FILE *filein=NULL;
-	filein = fopen(argv[3],"r");
-	while(fscanf(filein, "%lf", &lookup_table[i]) != EOF) i++;
-	if(i==16384) size = 14;
-	else if(i==32768) size = 15;
-	else if(i==65536) size = 16;
-	else if(i==131072) size = 17;
-	else if(i==262144) size = 18;
-	else if(i==524288) size = 19;
-	else if(i==1048576) size= 20;
+	FILE *fileinA=NULL;
+    FILE *fileinB=NULL;
+	fileinA = fopen(argv[3],"r");
+  	fileinB = fopen(argv[4],"r");
 
-        while (trace_read_packet(trace,packet)>0) {
+    //Read the .txt file and count how many lines in the file  
+	while(fscanf(fileinA, "%Lf", &lookup_tableA[lineA]) != EOF) lineA++;
+    //printf("lineA=%u\n",lineA);
+    while(fscanf(fileinB, "%Lf", &lookup_tableB[lineB]) != EOF) lineB++;
+    //printf("lineA=%u\n",lineB);
 
-		struct sockaddr_storage addr;
-		struct sockaddr *addr_ptr;
-		addr_ptr = trace_get_source_address(packet, (struct sockaddr *)&addr);
-		if (addr_ptr == NULL)
-			printf("NULL ");
-		else {
-			Y++;
-			int_ip = get_ip_int(addr_ptr);
-			//printf("%u\n",int_ip);
-			srand(int_ip);
-			for(i=0;i<K_VALUE;i++)
-			{
-				if(size==14) tmp = rand()%16384;
-				else if(size==15) tmp = rand()%32768;
-				else if(size==16) tmp = rand()%65536;
-				else if(size==17) tmp = rand()%131072;
-				else if(size==18) tmp = rand()%262144;
-				else if(size==19) tmp = rand()%524288;
-				else if(size==20) tmp = rand()%1048576;
-				//else if(size==K_VALUE) tmp &= 0x000fffff;
-				tmp2 = lookup_table[tmp];
-				z[i]+=tmp2;
-			}
-			if(Y==m)
-			{
-				for(i=0;i<K_VALUE;i++) z[i]/=m;
-				printf("%lf\n",H_function(z,1,m));
-				for(i=0;i<K_VALUE;i++) z[i]=0.0;
-				break;
-			}
-		}
-		
-		
-        }
+    while (trace_read_packet(trace,packet)>0) {
 
+        struct sockaddr_storage addr;
+        struct sockaddr *addr_ptr;
+        addr_ptr = trace_get_source_address(packet, (struct sockaddr *)&addr);
+        if (addr_ptr == NULL)
+            printf("NULL ");
+        else{
+            /* total packet count ++ */
+            Y++;
+            int_ip = get_ip_int(addr_ptr);
+            //printf("%u\n",int_ip);
+            /* seed the rand() */
+            srand(int_ip);
+            for(i=0;i<10;i++)
+            {
+                /* size = 14 */
+                if(lineA==16384) tmp = rand()%16384;
+                /* size = 15 */
+                else if(lineA==32768) tmp = rand()%32768;
+                /* size = 16 */
+                else if(lineA==65536) tmp = rand()%65536;
+                /* size = 17 */
+                else if(lineA==131072) tmp = rand()%131072;
+                /* size = 18 */
+                else if(lineA==262144) tmp = rand()%262144;
+                /* size = 19 */
+                else if(lineA==524288) tmp = rand()%524288;
+                /* size = 20 */
+                else if(lineA==1048576) tmp = rand()%1048576;
+                //else if(line==K_VALUE) tmp &= 0x000fffff;
+                tmp2 = lookup_tableA[tmp];
+                z[i]+=tmp2;
+            }
+            
+            for(i=11;i<20;i++)
+            {
+                /* size = 14 */
+                if(lineB==16384) tmp = rand()%16384;
+                /* size = 15 */
+                else if(lineB==32768) tmp = rand()%32768;
+                /* size = 16 */
+                else if(lineB==65536) tmp = rand()%65536;
+                /* size = 17 */
+                else if(lineB==131072) tmp = rand()%131072;
+                /* size = 18 */
+                else if(lineB==262144) tmp = rand()%262144;
+                /* size = 19 */
+                else if(lineB==524288) tmp = rand()%524288;
+                /* size = 20 */
+                else if(lineB==1048576) tmp = rand()%1048576;
+                //else if(line==K_VALUE) tmp &= 0x000fffff;
+                tmp2 = lookup_tableB[tmp];
+                z[i]+=tmp2;
+            }        
+            
+            /* Trace ended, print Entropy value and reset array*/
+            if(Y==m)
+            {
+                for(i=0;i<K_VALUE;i++) z[i]/=m;
+                printf("%Lf\n",H_function(z,1,m));
+                for(i=0;i<K_VALUE;i++) z[i]=0.0;
+                break;
+            }
+            }			
+    }
 
-        if (trace_is_err(trace)) {
-                trace_perror(trace,"Reading packets");
-                libtrace_cleanup(trace, packet);
-                return 1;
-        }
-
+    if (trace_is_err(trace)) {
+        trace_perror(trace,"Reading packets");
         libtrace_cleanup(trace, packet);
+        return 1;
+    }
 
-	printf("Y=%d, size=%d\n",Y,size);
+    libtrace_cleanup(trace, packet);
 
-        return 0;
+	printf("Y=%d, size=%lf\n",Y,log(lineA)/log(2));
+
+    return 0;
 }
